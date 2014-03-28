48257d2eee8c25c0c0b1c0d65210adee8ce061bc
Arturo Casal <berfenger [at] gmail.com>
2014-03-28 20:22:25 +0100

    PlayerActivity and PlayerService decoupled. This version is buildable and does the same.

--- src/com/radiobattletoads/player2/PlayerActivity.java
+++ src/com/radiobattletoads/player2/PlayerActivity.java
@@ -2,11 +2,12 @@ package com.radiobattletoads.player2;
 
 import java.util.Timer;
 
-import com.radiobattletoads.player2.DownloadCurrentinfo.DownloadCurrentInfoListener;
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils.TruncateAt;
 import android.util.Log;
 import android.view.Gravity;
@@ -20,15 +21,11 @@ import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
-import android.annotation.SuppressLint;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.Bitmap;
-import android.support.v7.app.ActionBarActivity;
 
-public class PlayerActivity extends ActionBarActivity implements DownloadCurrentInfoListener {
+import com.radiobattletoads.player2.DownloadCurrentinfo.DownloadCurrentInfoListener;
+import com.radiobattletoads.player2.PlayerService.PlayerStatusChangeListener;
+
+public class PlayerActivity extends ActionBarActivity implements DownloadCurrentInfoListener, PlayerStatusChangeListener {
 
 	public final static int MESSAGE_PLAYERSTATUS = 1;
 	public final static int MESSAGE_CURRENTPROGRAM = 2;
@@ -42,12 +39,17 @@ public class PlayerActivity extends ActionBarActivity implements DownloadCurrent
 	public static final String BROADCAST_PAUSE = "broadcast_pause";
 
 	private LinearLayout trackinfo_layout_container;
+	private TextView tv_status;
+	private LinearLayout bufferingLayout;
+	private Button playButton;
+	private Button pauseButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// Paint activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.player_layout);
+		PlayerService.register(this);
 
 		// Add timer to download current track info and initialize
 		status_trackinfo = STATUS_TRACKINFO_UNINITIALIZED;
@@ -57,11 +59,38 @@ public class PlayerActivity extends ActionBarActivity implements DownloadCurrent
 		// Initialize player looks and status
 		LayoutParams trackInfoParams = new LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, getWindowManager().getDefaultDisplay().getWidth() / 3 + 40);
 		findViewById(R.id.trackInfoLayout).setLayoutParams(trackInfoParams);
+		
+		// View cache
 		trackinfo_layout_container = (LinearLayout) findViewById(R.id.trackInfoLayout);
-		Message m = new Message();
-		m.what = PlayerActivity.MESSAGE_PLAYERSTATUS;
-		m.arg1 = PlayerService.status;
-		this.messageHandler.sendMessage(m);
+		tv_status = (TextView) findViewById(R.id.playerStatus);
+		bufferingLayout = (LinearLayout) findViewById(R.id.bufferingLayout);
+		playButton = (Button) findViewById(R.id.playbutton);
+		pauseButton = (Button) findViewById(R.id.pauseButton);
+		
+		int status = PlayerService.getAbsoluteStatus();
+		switch(status) {
+		case PlayerService.PLAYER_UNINITIALIZED:
+			this.onPlayerUninitialized();
+			break;
+		case PlayerService.PLAYER_BUFFERING:
+			this.onPlayerBuffering();
+			break;
+		case PlayerService.PLAYER_CONNECTIONPROBLEM_CUT:
+			this.onPlayerConnectionCut();
+			break;
+		case PlayerService.PLAYER_CONNECTIONPROBLEM_NOTSTARTED:
+			this.onPlayerConnectionError();
+			break;
+		case PlayerService.PLAYER_INITIALIZING:
+			this.onPlayerInitializing();
+			break;
+		case PlayerService.PLAYER_PLAYING:
+			this.onPlayerPlaying();
+			break;
+		case PlayerService.PLAYER_READY:
+			this.onPlayerReady();
+			break;
+		}
 
 		// Broadcast to receive notification actions
 		BroadcastReceiver receiver = new BroadcastReceiver() {
@@ -75,7 +104,12 @@ public class PlayerActivity extends ActionBarActivity implements DownloadCurrent
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(BROADCAST_PAUSE);
 		registerReceiver(receiver, filter);
-
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		PlayerService.unRegister(this);
 	}
 
 	@Override
@@ -85,140 +119,6 @@ public class PlayerActivity extends ActionBarActivity implements DownloadCurrent
 		inflater.inflate(R.menu.player_activity_actions, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
-
-	@SuppressLint("HandlerLeak")
-	Handler messageHandler = new Handler() {
-		@SuppressWarnings("deprecation")
-		public void handleMessage(Message m) {
-
-			TextView tv_status = (TextView) findViewById(R.id.playerStatus);
-			LinearLayout trackinfo_layout_container = (LinearLayout) findViewById(R.id.trackInfoLayout);
-			final float scale = getResources().getDisplayMetrics().density;
-			LinearLayout bufferingLayout = (LinearLayout) findViewById(R.id.bufferingLayout);
-			Button playButton = (Button) findViewById(R.id.playbutton);
-			Button pauseButton = (Button) findViewById(R.id.pauseButton);
-
-			switch (m.what) {
-			case MESSAGE_PLAYERSTATUS:
-				switch (m.arg1) {
-				case PlayerService.PLAYER_UNINITIALIZED:
-					tv_status.setText("Uninitialized");
-					bufferingLayout.setVisibility(View.GONE);
-					playButton.setVisibility(View.VISIBLE);
-					pauseButton.setVisibility(View.GONE);
-					break;
-				case PlayerService.PLAYER_READY:
-					tv_status.setText("Ready (idle)");
-					bufferingLayout.setVisibility(View.VISIBLE);
-					playButton.setVisibility(View.GONE);
-					pauseButton.setVisibility(View.GONE);
-					break;
-				case PlayerService.PLAYER_PLAYING:
-					tv_status.setText("Playing");
-					bufferingLayout.setVisibility(View.GONE);
-					playButton.setVisibility(View.GONE);
-					pauseButton.setVisibility(View.VISIBLE);
-					break;
-				case PlayerService.PLAYER_BUFFERING:
-					tv_status.setText("Buffering...");
-					bufferingLayout.setVisibility(View.VISIBLE);
-					playButton.setVisibility(View.GONE);
-					pauseButton.setVisibility(View.GONE);
-					break;
-				case PlayerService.PLAYER_INITIALIZING:
-					tv_status.setText("Initializing...");
-					bufferingLayout.setVisibility(View.VISIBLE);
-					playButton.setVisibility(View.GONE);
-					pauseButton.setVisibility(View.GONE);
-					break;
-				case PlayerService.PLAYER_CONNECTIONPROBLEM_NOTSTARTED:
-					tv_status.setText("Check your connection!");
-					bufferingLayout.setVisibility(View.GONE);
-					playButton.setVisibility(View.VISIBLE);
-					pauseButton.setVisibility(View.GONE);
-					break;
-				case PlayerService.PLAYER_CONNECTIONPROBLEM_CUT:
-					tv_status.setText("The stream ended!");
-					bufferingLayout.setVisibility(View.GONE);
-					playButton.setVisibility(View.VISIBLE);
-					pauseButton.setVisibility(View.GONE);
-					break;
-				}
-				break;
-			case MESSAGE_CURRENTPROGRAM:
-				Log.d("RBT", "Received info");
-				switch (m.arg1) {
-				case DownloadCurrentinfo.DOWNLOADCURRENTINFO_UPDATED:
-					Log.d("RBT", "Received downloaded info");
-
-					LinearLayout trackinfo_layout = new LinearLayout(PlayerActivity.this.getApplicationContext());
-					LinearLayout trackinfo_textlayout = new LinearLayout(PlayerActivity.this.getApplicationContext());
-					ImageView trackinfo_image = new ImageView(PlayerActivity.this.getApplicationContext());
-					TextView trackinfo_title = new TextView(PlayerActivity.this.getApplicationContext());
-					TextView trackinfo_description = new TextView(PlayerActivity.this.getApplicationContext());
-
-					trackinfo_layout.addView(trackinfo_image);
-					trackinfo_layout.addView(trackinfo_textlayout);
-
-					LayoutParams trackinfo_image_params = new LayoutParams(getWindowManager().getDefaultDisplay().getWidth() / 3, getWindowManager().getDefaultDisplay().getWidth() / 3, 1.0f);
-					trackinfo_image_params.setMargins(20, 20, 7, 20);
-					trackinfo_image.setLayoutParams(trackinfo_image_params);
-					trackinfo_image.setScaleType(ScaleType.FIT_CENTER);
-
-					LayoutParams trackinfo_textlayout_params = new LayoutParams(2 * (getWindowManager().getDefaultDisplay().getWidth() / 3), LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
-					trackinfo_textlayout_params.setMargins(7, 20, 20, 20);
-					trackinfo_textlayout.setLayoutParams(trackinfo_textlayout_params);
-
-					trackinfo_textlayout.setOrientation(LinearLayout.VERTICAL);
-					LayoutParams trackinfo_titledesc_params = new LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
-					trackinfo_title.setLayoutParams(trackinfo_titledesc_params);
-					trackinfo_textlayout.addView(trackinfo_title);
-					trackinfo_description.setLayoutParams(trackinfo_titledesc_params);
-					trackinfo_textlayout.addView(trackinfo_description);
-
-					trackinfo_title.setTextSize((int) (12 * scale + 0.5f));
-					trackinfo_title.setGravity(Gravity.BOTTOM);
-					trackinfo_description.setTextSize((int) (9 * scale + 0.5f));
-					trackinfo_description.setGravity(Gravity.TOP);
-					trackinfo_description.setMaxLines(2);
-					trackinfo_description.setEllipsize(TruncateAt.END);
-
-					trackinfo_title.setText(m.getData().getString("title"));
-					trackinfo_description.setText(m.getData().getString("description"));
-					trackinfo_image.setImageBitmap((Bitmap) m.getData().getParcelable("artwork"));
-
-					trackinfo_layout_container.removeViewAt(0);
-					trackinfo_layout_container.addView(trackinfo_layout);
-
-					break;
-				case DownloadCurrentinfo.DOWNLOADCURRENTINFO_FAILED:
-					Log.d("RBT", "Not received downloaded info. Connection failed?");
-					LinearLayout trackinfoerror_layout = new LinearLayout(PlayerActivity.this.getApplicationContext());
-					LayoutParams trackinfoerror_layout_params = new LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, getWindowManager().getDefaultDisplay().getWidth() / 3, 1.0f);
-					trackinfoerror_layout.setLayoutParams(trackinfoerror_layout_params);
-
-					LayoutParams trackinfoerror_textlayout_params = new LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-					trackinfoerror_textlayout_params.setMargins(20, 20, 20, 20);
-					TextView trackinfoerror_text = new TextView(PlayerActivity.this.getApplicationContext());
-					trackinfoerror_text.setTextSize((int) (12 * scale + 0.5f));
-					trackinfoerror_text.setGravity(Gravity.CENTER);
-					trackinfoerror_text.setText(R.string.cantdownloadinfo);
-					trackinfoerror_text.setLayoutParams(trackinfoerror_textlayout_params);
-
-					trackinfoerror_layout.addView(trackinfoerror_text);
-					trackinfo_layout_container.removeViewAt(0);
-					trackinfo_layout_container.addView(trackinfoerror_layout);
-
-					break;
-				case DownloadCurrentinfo.DOWNLOADCURRENTINFO_DOWNLOADING:
-
-					break;
-				}
-				break;
-			}
-
-		}
-	};
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -248,7 +148,7 @@ public class PlayerActivity extends ActionBarActivity implements DownloadCurrent
 
 		if (v.getId() == R.id.playbutton || v.getId() == R.id.pauseButton) {
 
-			if (PlayerService.status == PlayerService.PLAYER_UNINITIALIZED) {
+			if (PlayerService.getInstance() == null) {
 				// Initialize and play!
 				Log.d("RBT", "Play");
 				this.startService(new Intent(this, PlayerService.class));
@@ -335,6 +235,62 @@ public class PlayerActivity extends ActionBarActivity implements DownloadCurrent
 		trackinfoerror_layout.addView(trackinfoerror_text);
 		trackinfo_layout_container.removeViewAt(0);
 		trackinfo_layout_container.addView(trackinfoerror_layout);
+	}
+
+	@Override
+	public void onPlayerUninitialized() {
+		tv_status.setText("Uninitialized");
+		bufferingLayout.setVisibility(View.GONE);
+		playButton.setVisibility(View.VISIBLE);
+		pauseButton.setVisibility(View.GONE);
+	}
+
+	@Override
+	public void onPlayerReady() {
+		tv_status.setText("Ready (idle)");
+		bufferingLayout.setVisibility(View.VISIBLE);
+		playButton.setVisibility(View.GONE);
+		pauseButton.setVisibility(View.GONE);
+	}
+
+	@Override
+	public void onPlayerPlaying() {
+		tv_status.setText("Playing");
+		bufferingLayout.setVisibility(View.GONE);
+		playButton.setVisibility(View.GONE);
+		pauseButton.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	public void onPlayerBuffering() {
+		tv_status.setText("Buffering...");
+		bufferingLayout.setVisibility(View.VISIBLE);
+		playButton.setVisibility(View.GONE);
+		pauseButton.setVisibility(View.GONE);
+	}
+
+	@Override
+	public void onPlayerInitializing() {
+		tv_status.setText("Initializing...");
+		bufferingLayout.setVisibility(View.VISIBLE);
+		playButton.setVisibility(View.GONE);
+		pauseButton.setVisibility(View.GONE);
+	}
+
+	@Override
+	public void onPlayerConnectionError() {
+		tv_status.setText("Check your connection!");
+		bufferingLayout.setVisibility(View.GONE);
+		playButton.setVisibility(View.VISIBLE);
+		pauseButton.setVisibility(View.GONE);
+	}
+
+	@Override
+	public void onPlayerConnectionCut() {
+		tv_status.setText("The stream ended!");
+		bufferingLayout.setVisibility(View.GONE);
+		playButton.setVisibility(View.VISIBLE);
+		pauseButton.setVisibility(View.GONE);
 	}
 
 }
