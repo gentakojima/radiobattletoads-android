9b231b793830e9b2b8a511ed582f498a34f454fc
Jorge Suárez de Lis <yo@jorgesuarezdelis.name>
2014-03-30 15:27:26 +0200

    Add setting to enable/disable notifications. Partially implements #2. This is incomplete, since the notifications are not enabled/disabled instantly when the radio is being played. Some work has been done on PreferencesActivity, but it seems that I need to register that listener somewhere. Not idea on how to do that for now.

--- src/com/radiobattletoads/player2/PreferencesActivity.java
+++ src/com/radiobattletoads/player2/PreferencesActivity.java
@@ -1,6 +1,8 @@
 package com.radiobattletoads.player2;
 
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
@@ -16,7 +18,18 @@ public class PreferencesActivity extends PreferenceActivity implements OnSharedP
     }
 	
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		
+		Log.d("RBT", "Called onSharedPreferenceChanged");
+		if(key=="notification"){
+			Log.d("RBT", "onSharedPreferenceChanged: notification");
+			Boolean enabled = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("notification", true);
+			if(enabled){
+				RBTPlayerApplication.getFromContext(this.getBaseContext()).getNotifications().enable();
+			}
+			else{
+				RBTPlayerApplication.getFromContext(this.getBaseContext()).getNotifications().disable();				
+			}
+			
+		}
 		
 	}
 
