9e52c5650381f1d492d183b84e29e3bb6e2f729d
Jorge Suárez de Lis <yo@jorgesuarezdelis.name>
2014-08-28 18:15:27 +0200

    Oops! Forgot to add some files on other commits.

---
+++ src/com/radiobattletoads/player2/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.radiobattletoads.player2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_layout);
+        
+    }
+
+}
